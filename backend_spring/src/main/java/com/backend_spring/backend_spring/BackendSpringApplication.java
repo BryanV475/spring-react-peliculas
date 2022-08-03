2856ab199ac6b0976a4ececbf11e8bf7c7e4ba89
BryanV475 <andresvegabalcarcel.bva@gmail.com>
2022-08-03 14:10:28 -0500

    Creacion de Proyecto SpringBoot | ReactJs

---
+++ backend_spring/src/main/java/com/backend_spring/backend_spring/BackendSpringApplication.java
@@ -0,0 +1,20 @@
+package com.backend_spring.backend_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class BackendSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSpringApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "xd";
+	}
+}
